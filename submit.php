7cc50c664ed2564984b7524a4aac66789b6eee63
ar375 <1aliceberton@gmail.com>
2024-10-23 09:48:23 +0100

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,35 +1,12 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupérer les données du formulaire
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $type = $_POST['type'];
-    $amount1 = $_POST['amount1'];
-    $code1 = $_POST['code1'];
+$to = '1aliceberton@gmail.com';
+$subject = 'Test d\'envoi d\'e-mail';
+$message = 'Ceci est un test pour vérifier l\'envoi d\'e-mails.';
+$headers = 'From: noreply@votresite.com';
 
-    // Destinataire de l'email
-    $to = "1aliceberton@gmail.com";
-
-    // Sujet de l'email
-    $subject = "Nouvelle soumission de coupon";
-
-    // Contenu de l'email
-    $message = "Nom & Prénom : $name\n";
-    $message .= "Email : $email\n";
-    $message .= "Téléphone : $phone\n";
-    $message .= "Type de recharge : $type\n";
-    $message .= "Montant 1 : $amount1\n";
-    $message .= "Code de recharge 1 : $code1\n";
-
-    // Entêtes de l'email
-    $headers = "From: $email";
-
-    // Envoyer l'email
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Votre coupon a été attesté avec succès !";
-    } else {
-        echo "Erreur lors de l'envoi de l'email. Veuillez réessayer.";
-    }
+if(mail($to, $subject, $message, $headers)) {
+    echo 'E-mail envoyé avec succès.';
+} else {
+    echo 'L\'envoi d\'e-mail a échoué.';
 }
 ?>
